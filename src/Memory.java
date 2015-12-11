8e1fa7bf417035a154b77ba5b58d8030fd9dc08c
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2015-12-11 23:13:20 +0000

    Back up commit (before adding the writeback stage).
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/Memory.java

--- src/Memory.java
+++ src/Memory.java
@@ -226,7 +226,7 @@ public class Memory
     
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     /* N-th Fibonacci number correct way to write it in assembly */
-    /*
+    ///*
     memoryArray[0] = Utility.convertToInt("10001000000000000000000000010001", true); // MOVI R1, 1 (Move 1 into R1)
     memoryArray[1] = Utility.convertToInt("10001000000000000000000000010010", true); // MOVI R2, 1 (Move 1 into R2)
     memoryArray[2] = Utility.convertToInt("10001000000000000000000000110011", true); // MOVI R3, 3 (Move 3 into R3)
@@ -239,8 +239,7 @@ public class Memory
     memoryArray[9] = Utility.convertToInt("00101000000000000000000100110011", true); // ADDI R3, R3, 1 (Add 1 to R3 and store the result in R3)
     memoryArray[10] = Utility.convertToInt("10011000000000001111111111111011", true); // BU -5
     memoryArray[11] = Utility.convertToInt("10001000000000000000111111111111", true); // MOVI R16, 255 (Move 255 into R16)  - SVC call to suspend/halt the program being simulated 
-    */
-    
+    //*/
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -259,6 +258,9 @@ public class Memory
     */
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
   
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /* Used to test the BL and RET instructions */
+    /*
     memoryArray[0] = Utility.convertToInt("10001000000000000001001011000001", true); // MOVI R1, 1 (Move 300 into R1)
     memoryArray[1] = Utility.convertToInt("00000000000000000000000000000000", true); // NOP (Inserting a NOP to avoid control dependency hazard) 
     memoryArray[2] = Utility.convertToInt("00000000000000000000000000000000", true); // NOP (Inserting a NOP to avoid control dependency hazard) 
@@ -272,7 +274,8 @@ public class Memory
     memoryArray[15] = Utility.convertToInt("00000000000000000000000000000000", true); // NOP (Inserting a NOP to avoid control dependency hazard)
     // Note need at least three NOP instruction's before returning from the method.
     memoryArray[16] = Utility.convertToInt("11001000000000000000000000000000", true); // RET (Return from function call)
-
+    */
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////
   }
 
   /**
